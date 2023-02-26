9e22ee62f74e347fb5c760d5bd949360e509ac96
Bao <baontde150158@fpt.edu.vn>
2023-02-26 18:21:20 +0700

    Add project files.

---
+++ ASPNETCoreWebAPIJWT/Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+﻿using ASPNETCoreWebAPIJWT.Model;
+using ASPNETCoreWebAPIJWT.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace ASPNETCoreWebAPIJWT.Controllers
+{
+    [Authorize]
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IJWTManagerRepository _jWTManager;
+
+        public UsersController(IJWTManagerRepository jWTManager)
+        {
+            this._jWTManager = jWTManager;
+        }
+
+        [HttpGet]
+        public List<string> Get()
+        {
+            var users = new List<string>
+        {
+            "Thai Bao",
+            "Loc Tan",
+            "Huu Tuan"
+        };
+
+            return users;
+        }
+
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("authenticate")]
+        public IActionResult Authenticate(Users usersdata)
+        {
+            var token = _jWTManager.Authenticate(usersdata);
+            if (token == null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(token);
+        }
+    }
+}
---
+++ ASPNETCoreWebAPIJWT/Repository/IJWTManagerRepository.cs
@@ -0,0 +1,9 @@
+﻿using ASPNETCoreWebAPIJWT.Model;
+
+namespace ASPNETCoreWebAPIJWT.Repository
+{
+    public interface IJWTManagerRepository
+    {
+        Tokens Authenticate(Users users);
+    }
+}
